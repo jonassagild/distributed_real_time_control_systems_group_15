941e12aae24c65edcf4722c068641c496520c408
Jonas Åsnes  Sagild <jonassagild@hotmail.com>
2018-12-04 17:49:41 +0000

    adds exit after first loop

--- distributed/session_8/session_8.ino
+++ distributed/session_8/session_8.ino
@@ -4,13 +4,14 @@ void setup() {
   Serial.begin(115200);
   // void initialize_system(double _k_11, double _k_12, double _k_21, double _k_22, _l, _o, _c, _rho, _i2c_master_address);
 
-  initialize_system(2, 1, 1, 2, 150, 30, 1, 0.07, 26);
+  initialize_system(2, 1, 1, 2, 80, 0, 1, 0.07, 25);
   initialize_node();
   
 }
 
 void loop() {
-  
+
   consens();
+  exit(0);
   
 }
